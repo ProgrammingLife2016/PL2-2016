0796067bb21c2674584793c70bd0e84b55a584c3
FarisElghlan <faris.elghlan@hotmail.com>
2016-04-21 14:25:54 +0200

    Created the skeleton of the UI. This includes the fxml file of the initial view, code to load the view and the controller class of the view.

--- Dnav/src/dnav/Dnav.java
+++ Dnav/src/dnav/Dnav.java
@@ -1,6 +1,12 @@
 package dnav;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -8,16 +14,45 @@ import javafx.stage.Stage;
  * @author Faris
  */
 public class Dnav extends Application {
-    
+
+    /**
+     * Start the application. This method is automatically called by JavaFX when the API is
+     * initialized, after the call to launch(args) in the main method.
+     *
+     * @param primaryStage the primary stage of the application.
+     */
     @Override
     public void start(Stage primaryStage) {
+        Scene scene = new Scene(loadUi(), 600, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     /**
+     * Load the initial view of the user interface.
+     *
+     * @return the pane containing the initial view of the user interface.
+     */
+    private static Pane loadUi() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Dnav.class.getResource("view/RootLayout.fxml"));
+        try {
+            Pane pane = loader.load();
+            return pane;
+        } catch (IOException ex) {
+            Logger.getLogger(Dnav.class.getName()).log(Level.SEVERE, "Failed to load fxml file: RootLayout.fxml", ex);
+            return null;
+        }
+    }
+
+    /**
+     * Launch the application.
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
---
+++ Dnav/src/proto/RootLayoutController.java
@@ -0,0 +1,34 @@
+package proto;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Faris
+ */
+public class RootLayoutController {
+
+    @FXML
+    private StackPane spane;
+    @FXML
+    private Pane graphPane;
+    @FXML
+    private StackPane locationIdentifierPane;
+    @FXML
+    private Rectangle locationIdentifierRectangle;
+    @FXML
+    private Slider zoomInSlider;
+    
+    /**
+     * Initializes the controller class.
+     */
+    public void initialize() {
+        // TODO
+    }
+
+}
