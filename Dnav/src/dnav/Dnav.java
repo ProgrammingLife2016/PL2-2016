93b5d7aafa09d24ae7972d7aea0a6ec097088cf7
FarisElghlan <faris.elghlan@hotmail.com>
2016-04-21 14:07:35 +0200

    Added temporary empty project to work in till maven is set up correctly.

---
+++ Dnav/src/dnav/Dnav.java
@@ -0,0 +1,23 @@
+package dnav;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Faris
+ */
+public class Dnav extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
