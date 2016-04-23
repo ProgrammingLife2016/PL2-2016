76fc38986028b3dec1cde4cb952eaf4e27aa6ec1
FarisElghlan <faris.elghlan@hotmail.com>
2016-04-23 16:52:55 +0200

    Made zooming out more smooth and disabled zooming during the animation.

--- Dnav/src/dnav/view/graph/TreeController.java
+++ Dnav/src/dnav/view/graph/TreeController.java
@@ -1,6 +1,7 @@
 package dnav.view.graph;
 
 import dnav.model.TreeNode;
+import javafx.animation.Timeline;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -16,6 +17,7 @@ public class TreeController {
     private final Pane graphPane;
     private final Button zoomOutButton;
     private ViewNode currentRoot;
+    boolean isZooming = false;
 
     /**
      * Create a new tree controller which manages the pholygenetic tree.
@@ -27,7 +29,8 @@ public class TreeController {
     public TreeController(Pane graphPane, TreeNode root, Button zoomOutButton) {
         this.graphPane = graphPane;
         this.zoomOutButton = zoomOutButton;
-        initializeZoomEventHandlers();
+        initializeZoomInEventHandler();
+        initializeZoomOutEventHandler();
         initializeGraphSizeListeners();
         setRoot(root);
     }
@@ -36,19 +39,41 @@ public class TreeController {
      * Initialize the event handlers which zoom in when a node is clicked or zoom out when the zoom
      * out button is clicked.
      */
-    private void initializeZoomEventHandlers() {
+    private void initializeZoomOutEventHandler() {
         zoomOutButton.setOnAction(e -> {
+            if (isZooming) {
+                return;
+            }
             if (currentRoot.getDataNode().hasParent()) {
-//                setRoot(currentRoot.getDataNode().getParent());
-                currentRoot.zoomOut(currentRoot);
+                Timeline timeline = new Timeline();
+                currentRoot.zoomOut(currentRoot, timeline);
+                timeline.setOnFinished(e2 -> {
+                    setRoot(currentRoot.getDataNode().getParent());
+                    isZooming = false;
+                });
+                isZooming = true;
+                timeline.play();
             } else {
                 zoomOutButton.setDisable(true);
             }
         });
+    }
+
+    private void initializeZoomInEventHandler() {
         graphPane.setOnMouseClicked((MouseEvent event) -> {
+            if (isZooming) {
+                return;
+            }
             if (event.getTarget().getClass().equals(ViewNode.class)) {
                 ViewNode clickedNode = (ViewNode) event.getTarget();
-                currentRoot.zoomIn(clickedNode);
+                Timeline timeline = new Timeline();
+                currentRoot.zoomIn(clickedNode, timeline);
+                timeline.setOnFinished(e -> {
+                    setRoot(clickedNode.getDataNode());
+                    isZooming = false;
+                });
+                isZooming = true;
+                timeline.play();
             }
         });
     }
