d43bbc3b3e52854815126a4b86ea74079607bd6c
FarisElghlan <faris.elghlan@hotmail.com>
2016-04-23 15:04:47 +0200

    Added comments and deleted unused code.

--- Dnav/src/dnav/view/graph/ViewNode.java
+++ Dnav/src/dnav/view/graph/ViewNode.java
@@ -30,6 +30,14 @@ public class ViewNode extends Circle {
     private GraphArea graphArea;
     private boolean isLeaf = false;
 
+    /**
+     * Create a view node.
+     *
+     * @param dataNode the data of the node.
+     * @param parent the parent view node.
+     * @param graphArea the graph area in which the node has to be drawn.
+     * @param controller the controller of the tree.
+     */
     private ViewNode(TreeNode dataNode, ViewNode parent, GraphArea graphArea,
             TreeController controller) {
         super(NODE_RADIUS);
@@ -47,10 +55,22 @@ public class ViewNode extends Circle {
         this.setCenterY(graphArea.getCenterY());
     }
 
+    /**
+     * Get the data of this node.
+     *
+     * @return the data of this node.
+     */
     protected TreeNode getDataNode() {
         return dataNode;
     }
 
+    /**
+     * Draw a root node and all of its children which fit on the screen.
+     *
+     * @param root the root node.
+     * @param controller the controller of the tree.
+     * @return the view node of the root.
+     */
     protected static ViewNode drawRootNode(TreeNode root, TreeController controller) {
         Pane graphPane = controller.getGraphPane();
         double startX = TreeController.GRAPH_BORDER_OFFSET;
@@ -61,6 +81,15 @@ public class ViewNode extends Circle {
         return drawNode(root, null, gbox, controller);
     }
 
+    /**
+     * Recursively draw the node and all of its children.
+     *
+     * @param dataNode the data of the node to draw.
+     * @param parent the parent of the node to draw.
+     * @param graphArea the area in which the node should be drawn.
+     * @param controller the controller of the tree.
+     * @return the drawn view node.
+     */
     private static ViewNode drawNode(TreeNode dataNode, ViewNode parent, GraphArea graphArea,
             TreeController controller) {
         if (graphArea.getWidth() < NODE_DIAMETER || graphArea.getHeight() < NODE_DIAMETER
@@ -69,13 +98,6 @@ public class ViewNode extends Circle {
         }
         ViewNode node = new ViewNode(dataNode, parent, graphArea, controller);
         controller.getGraphPane().getChildren().add(node);
-        return redrawNode(node);
-    }
-
-    private static ViewNode redrawNode(ViewNode node) {
-        TreeNode dataNode = node.dataNode;
-        GraphArea graphArea = node.graphArea;
-        TreeController controller = node.controller;
         double nextStartX = graphArea.getCenterX();
         double ySize = (graphArea.endY - graphArea.startY) / dataNode.getDirectChildCount();
         for (int i = 0; i < dataNode.getDirectChildCount(); i++) {
@@ -96,6 +118,13 @@ public class ViewNode extends Circle {
         return node;
     }
 
+    /**
+     * Draw the edge between a parent and a child node.
+     *
+     * @param parent the parent node.
+     * @param child the child node.
+     * @param graphPane the pane in which the edge should be drawn.
+     */
     private static void drawEdge(ViewNode parent, ViewNode child, Pane graphPane) {
         Line edge = new Line();
         edge.startXProperty().bind(parent.centerXProperty());
@@ -107,6 +136,13 @@ public class ViewNode extends Circle {
         child.edge = edge;
     }
 
+    /**
+     * Draw an elipsis after a parent node to indicate that the parent has children, but there is
+     * not enough space to draw the children.
+     *
+     * @param node the parent node.
+     * @param graphPane the pane in which to draw the elipsis.
+     */
     private static void drawElipsis(ViewNode node, Pane graphPane) {
         Line l = new Line();
         l.startXProperty().bind(node.centerXProperty().add(node.getRadius() * 2));
@@ -118,15 +154,27 @@ public class ViewNode extends Circle {
         node.elipsis = l;
     }
 
-    public void zoomIn(ViewNode oldRoot, ViewNode newRoot) {
+    /**
+     * Zoom in on a node and animate the zooming proces.
+     *
+     * @param newRoot the node which should be the root after zooming in.
+     */
+    public void zoomIn(ViewNode newRoot) {
         Timeline tl = new Timeline();
-        zoomIn(oldRoot.graphArea, newRoot.graphArea, tl);
+        zoomIn(this.graphArea, newRoot.graphArea, tl);
         tl.setOnFinished(e -> {
             controller.setRoot(newRoot.dataNode);
         });
         tl.play();
     }
 
+    /**
+     * Zoom in on a node and animate the zooming proces.
+     *
+     * @param originalArea the area of the currently drawn tree.
+     * @param zoomArea the area of the tree to zoom in on.
+     * @param timeline the timeline which is used for the animation.
+     */
     private void zoomIn(GraphArea originalArea, GraphArea zoomArea, Timeline timeline) {
         double newX = (getCenterX() - zoomArea.startX - NODE_RADIUS);
         newX = newX / zoomArea.getWidth() * originalArea.getWidth();
@@ -143,4 +191,12 @@ public class ViewNode extends Circle {
             child.zoomIn(originalArea, zoomArea, timeline);
         }
     }
+
+//    public void zoomOut(ViewNode oldRoot, TreeNode newRoot) {
+//        
+//    }
+//    
+//    private void zoomOut(GraphArea originalArea, GraphArea zoomArea, Timeline timeline) {
+//        
+//    }
 }
