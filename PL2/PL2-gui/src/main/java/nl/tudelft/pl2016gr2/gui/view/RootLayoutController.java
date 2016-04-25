7163f1740099a0cc287722d1d0e5d1585214da8e
Justin van der Krieken <justin@vdkrieken.com>
2016-04-25 14:01:40 +0200

    migrated java files
    This code is able to run, packages are renamed etc

---
+++ PL2/PL2-gui/src/main/java/nl/tudelft/pl2016gr2/gui/view/RootLayoutController.java
@@ -0,0 +1,50 @@
+package nl.tudelft.pl2016gr2.gui.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import nl.tudelft.pl2016gr2.gui.model.IPhylogeneticTreeNode;
+import nl.tudelft.pl2016gr2.gui.model.TreeNode;
+import nl.tudelft.pl2016gr2.gui.view.graph.TreeController;
+
+/**
+ * FXML Controller class.
+ *
+ * @author Faris
+ */
+public class RootLayoutController {
+
+    @FXML
+    private Pane graphPane, heatmapPane;
+    @FXML
+    private Button zoomOutButton;
+    @FXML
+    private StackPane locationIdentifierPane;
+    @FXML
+    private Rectangle locationIdentifierRectangle;
+
+    private TreeNode currentRoot;
+
+    private static RootLayoutController controller;
+    private TreeController treeController;
+
+    /**
+     * Initializes the controller class.
+     */
+    public void initialize() {
+        assert (controller == null);
+        controller = this;
+    }
+
+    /**
+     * Set the data which has to be visualized.
+     *
+     * @param root the root of the tree which has to be drawn.
+     */
+    public void setData(IPhylogeneticTreeNode root) {
+        assert treeController == null;
+        treeController = new TreeController(graphPane, root, zoomOutButton);
+    }
+}
