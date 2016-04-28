98ec8a40b9eac158751557b3fb60c6450984b4dc
FarisElghlan <faris.elghlan@hotmail.com>
2016-04-28 17:09:12 +0200

    Moved the graph visualization to a separate class, improved the y position of the nodes and the code is using the ids as ids and not as indexes in the list anymore.

---
+++ PL2/PL2-gui/src/main/java/nl/tudelft/pl2016gr2/gui/view/tempGraph/DrawGraph.java
@@ -0,0 +1,148 @@
+package nl.tudelft.pl2016gr2.gui.view.tempGraph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import static nl.tudelft.pl2016gr2.core.algorithms.AlgoRunner.FILENAME;
+import static nl.tudelft.pl2016gr2.core.algorithms.AlgoRunner.GRAPH_SIZE;
+import nl.tudelft.pl2016gr2.model.Bubble;
+import nl.tudelft.pl2016gr2.model.Graph;
+import nl.tudelft.pl2016gr2.parser.controller.GFAReader;
+
+/**
+ *
+ * @author faris
+ */
+public class DrawGraph {
+
+	private static final double X_OFFSET = 50.0;
+
+	public void drawGraph(Stage primaryStage) {
+		Pane pane = new Pane();
+		ScrollPane scrollPane = new ScrollPane(pane);
+		double paneHeight = 600;
+		Scene scene = new Scene(scrollPane, 1000, paneHeight);
+		primaryStage.setMinHeight(400);
+		primaryStage.setMinWidth(600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		long s = System.currentTimeMillis();
+		GFAReader r = new GFAReader(FILENAME, GRAPH_SIZE);
+		Graph g = r.getGraph();
+		System.out.println("Size of the graph: " + g.getSize());
+		long e = System.currentTimeMillis();
+		System.out.println("The loading took " + (e - s) + " milliseconds to run");
+		s = e;
+//		FindBubbles findBubbles = new FindBubbles(g);
+//		findBubbles.calculateBubbles();
+//		long f = System.currentTimeMillis();
+//		System.out.println("The algorithm took " + (f - e) + " milliseconds to run");
+
+		HashMap<Integer, Bubble> bubbles = new HashMap<>();
+		ArrayList<Bubble> rawBubbles = g.getNodes();
+		for (int i = 1; i < rawBubbles.size(); i++) { // skip first
+			bubbles.put(rawBubbles.get(i).getId(), rawBubbles.get(i));
+		}
+		HashMap<Integer, Circle> circles = drawCircles(pane, bubbles);
+		drawEdges(pane, bubbles, circles);
+
+		ArrayList<ArrayList<Bubble>> bubbleDepths = createGraphDepth(g.getRoot(), bubbles);
+		int xPos = 0;
+		for (ArrayList<Bubble> bubbleDepth : bubbleDepths) {
+			xPos += X_OFFSET;
+			double bubbleAreaHeight = paneHeight / bubbleDepth.size();
+			for (int i = 0; i < bubbleDepth.size(); i++) {
+				Bubble bubble = bubbleDepth.get(i);
+				double startY = bubbleAreaHeight * i;
+				double endY = startY + bubbleAreaHeight;
+				double centerY = (endY + startY) / 2.0;
+				Circle c = circles.get(bubble.getId());
+				c.setCenterX(xPos);
+				c.setCenterY(centerY);
+			}
+		}
+		e = System.currentTimeMillis();
+		System.out.println("Drawing took " + (e - s) + " milliseconds");
+	}
+
+	private HashMap<Integer, Circle> drawCircles(Pane pane, HashMap<Integer, Bubble> bubbles) {
+		HashMap<Integer, Circle> circles = new HashMap<>();
+		bubbles.forEach((Integer id, Bubble bubble) -> {
+			Circle c = new Circle(15);
+			circles.put(id, c);
+			pane.getChildren().add(c);
+			Label label = new Label(Integer.toString(bubble.getId()));
+			label.layoutXProperty().bind(c.centerXProperty().add(-c.getRadius() + 3));
+			label.layoutYProperty().bind(c.centerYProperty().add(-c.getRadius() / 2));
+			label.setTextFill(new javafx.scene.paint.Color(1, 1, 1, 1));
+			pane.getChildren().add(label);
+		});
+		return circles;
+	}
+
+	private void drawEdges(Pane pane, HashMap<Integer, Bubble> bubbles, HashMap<Integer, Circle> circles) {
+		bubbles.forEach((Integer id, Bubble from) -> {
+			Circle fromCircle = circles.get(id);
+			for (Integer outLink : from.getOutLinks()) {
+				Circle toCircle = circles.get(outLink);
+				Line edge = new Line();
+				edge.startXProperty().bind(fromCircle.centerXProperty());
+				edge.startYProperty().bind(fromCircle.centerYProperty());
+				edge.endXProperty().bind(toCircle.centerXProperty());
+				edge.endYProperty().bind(toCircle.centerYProperty());
+				pane.getChildren().add(edge);
+				edge.toBack();
+//				edge.setSmooth(true);
+			}
+		});
+	}
+
+	private ArrayList<ArrayList<Bubble>> createGraphDepth(Bubble root, HashMap<Integer, Bubble> bubbles) {
+		HashMap<Integer, BubbleDepth> bubbleDepths = new HashMap<>();
+		bubbles.forEach((Integer id, Bubble bubble) -> {
+			bubbleDepths.put(id, new BubbleDepth(bubble));
+		});
+		Set currentBubbles = new HashSet<>();
+		currentBubbles.add(root.getId());
+		int depth = 0;
+		while (!currentBubbles.isEmpty()) {
+			Set nextBubbles = new HashSet();
+			Iterator<Integer> it = currentBubbles.iterator();
+			while (it.hasNext()) {
+				int currentBubble = it.next();
+				bubbleDepths.get(currentBubble).depth = depth;
+				nextBubbles.addAll(bubbles.get(currentBubble).getOutLinks());
+			}
+			currentBubbles = nextBubbles;
+			depth++;
+		}
+		ArrayList<ArrayList<Bubble>> res = new ArrayList<>();
+		for (int i = 0; i < depth; i++) {
+			res.add(new ArrayList<>());
+		}
+		bubbleDepths.forEach((Integer id, BubbleDepth bubbleDepth) -> {
+			res.get(bubbleDepth.depth).add(bubbleDepth.bubble);
+		});
+		return res;
+	}
+
+	private class BubbleDepth {
+
+		private final Bubble bubble;
+		private int depth;
+
+		public BubbleDepth(Bubble bubble) {
+			this.bubble = bubble;
+		}
+	}
+}
--- PL2/PL2-launcher/src/main/java/nl/tudelft/pl2016gr2/launcher/Dnav.java
+++ PL2/PL2-launcher/src/main/java/nl/tudelft/pl2016gr2/launcher/Dnav.java
@@ -1,35 +1,17 @@
 package nl.tudelft.pl2016gr2.launcher;
 
-import java.awt.Color;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import nl.tudelft.pl2016gr2.core.algorithms.AlgoRunner;
 import nl.tudelft.pl2016gr2.gui.model.TreeNode;
 import nl.tudelft.pl2016gr2.gui.view.RootLayoutController;
-import nl.tudelft.pl2016gr2.model.Bubble;
-import nl.tudelft.pl2016gr2.model.Graph;
-import nl.tudelft.pl2016gr2.parser.controller.GFAReader;
+import nl.tudelft.pl2016gr2.gui.view.tempGraph.DrawGraph;
 
 /**
  *
  * @author Faris
  */
 public class Dnav extends Application {
-
-	public static final String FILENAME = AlgoRunner.class.getClassLoader().getResource("TB10.gfa").getFile();
-	public static final int GRAPH_SIZE = 8728;
-	private static final double X_OFFSET = 50.0;
 
 	/**
 	 * Start the application. This method is automatically called by JavaFX when
@@ -54,88 +36,7 @@ public class Dnav extends Application {
 //
 //		insertData(controller);
 
-		Pane pane = new Pane();
-		ScrollPane scrollPane = new ScrollPane(pane);
-		double paneHeight = 800;
-		Scene scene = new Scene(scrollPane, 1000, 800);
-		primaryStage.setMinHeight(400);
-		primaryStage.setMinWidth(600);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-		long s = System.currentTimeMillis();
-		GFAReader r = new GFAReader(FILENAME, GRAPH_SIZE);
-		Graph g = r.getGraph();
-		System.out.println("Size of the graph: " + g.getSize());
-		long e = System.currentTimeMillis();
-		System.out.println("The loading took " + (e - s) + " milliseconds to run");
-//		FindBubbles findBubbles = new FindBubbles(g);
-//		findBubbles.calculateBubbles();
-//		long f = System.currentTimeMillis();
-//		System.out.println("The algorithm took " + (f - e) + " milliseconds to run");
-
-		ArrayList<Bubble> bubbles = g.getNodes();
-		bubbles.remove(0);
-		ArrayList<Circle> circles = new ArrayList<>();
-		// create a circle for each bubble
-		for (Bubble bubble : bubbles) {
-			Circle c = new Circle(15);
-			circles.add(c);
-			pane.getChildren().add(c);
-			Label label = new Label(Integer.toString(bubble.getId()));
-			label.layoutXProperty().bind(c.centerXProperty().add(-c.getRadius() / 2));
-			label.layoutYProperty().bind(c.centerYProperty().add(-c.getRadius() / 2));
-			label.setTextFill(new javafx.scene.paint.Color(1, 1, 1, 1));
-			pane.getChildren().add(label);
-		}
-		// create lines for each edge between bubbles
-		for (int i = 0; i < circles.size(); i++) {
-			Bubble from = bubbles.get(i);
-			Circle fromCircle = circles.get(i);
-			for (Integer outLink : from.getOutLinks()) {
-				Circle toCircle = circles.get(outLink - 1);
-				Line edge = new Line();
-				edge.startXProperty().bind(fromCircle.centerXProperty());
-				edge.startYProperty().bind(fromCircle.centerYProperty());
-				edge.endXProperty().bind(toCircle.centerXProperty());
-				edge.endYProperty().bind(toCircle.centerYProperty());
-				pane.getChildren().add(edge);
-			}
-		}
-//		Circle rootCircle = circles.get(g.getRoot().getId() - 1);
-//		rootCircle.setCenterX(X_OFFSET);
-//		rootCircle.setCenterY(pane.getHeight() / 2.0);
-		Set currentBubbles = new HashSet<>();
-		currentBubbles.add(g.getRoot().getId() - 1);
-		double curXPos = 0;
-		while (!currentBubbles.isEmpty()) {
-			curXPos += X_OFFSET;
-			Set nextBubbles = new HashSet();
-//			ArrayList<Integer> nextBubbles = new ArrayList<>();
-			double bubbleAreaHeight = paneHeight / currentBubbles.size();
-			Iterator<Integer> it = currentBubbles.iterator();
-			for (int i = 0; it.hasNext(); i++) {
-				int currentBubble = it.next() - 1;
-				Circle c = circles.get(currentBubble);
-				double startY = bubbleAreaHeight * i;
-				double endY = startY + bubbleAreaHeight;
-				double centerY = (endY + startY) / 2.0;
-				c.setCenterY(centerY);
-				c.setCenterX(curXPos);
-				nextBubbles.addAll(setBubblePositions(
-						bubbles.get(currentBubble), circles, curXPos, centerY
-				));
-			}
-			currentBubbles = nextBubbles;
-		}
-	}
-
-	private ArrayList<Integer> setBubblePositions(Bubble currentBubble,
-			ArrayList<Circle> circles, double xPos, double yPos) {
-		Circle c = circles.get(currentBubble.getId() - 1);
-		c.setCenterX(xPos);
-		c.setCenterY(yPos);
-		return currentBubble.getOutLinks();
+		new DrawGraph().drawGraph(primaryStage);
 	}
 
 	private void insertData(RootLayoutController controller) {
