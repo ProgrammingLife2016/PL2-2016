f6b0dc5fcabb05dd035a13f9e922d1989d394458
Casper Athmer <casperathmer92@gmail.com>
2016-04-28 09:28:06 +0200

    Changed node and bubble class

--- PL2/PL2-shared/src/main/java/nl/tudelft/pl2016gr2/model/Bubble.java
+++ PL2/PL2-shared/src/main/java/nl/tudelft/pl2016gr2/model/Bubble.java
@@ -1,9 +1,39 @@
 package nl.tudelft.pl2016gr2.model;
 
+import java.util.ArrayList;
+
 
 public class Bubble {
 	
 	private int level;
+	private int id;
+	private ArrayList<Bubble> nestedBubbles;
+	private ArrayList<Bubble> inLinks;
+	private ArrayList<Bubble> outLinks;
+	private Bubble startBubble;
+	private Bubble endBubble;
+	
+	public Bubble(int id) {
+		this.id = id;
+		nestedBubbles = new ArrayList<>();
+		inLinks = new ArrayList<>();
+		outLinks = new ArrayList<>();
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (object instanceof Bubble) {
+			Bubble bubble = (Bubble) object;
+			
+			return bubble.id == this.id;
+		}
+		
+		return false;
+	}
+	
+	public int getId() {
+		return id;
+	}
 	
 	public int getLevel() {
 		return level;
@@ -11,6 +41,46 @@ public class Bubble {
 	
 	public void setLevel(int level) {
 		this.level = level;
+	}
+	
+	public void setStartBubble(Bubble bubble) {
+		this.startBubble = bubble;
+	}
+	
+	public Bubble getStartBubble() {
+		return startBubble;
+	}
+	
+	public void setEndBubble(Bubble bubble) {
+		this.endBubble = bubble;
+	}
+	
+	public Bubble getEndBubble() {
+		return endBubble;
+	}
+	
+	public ArrayList<Bubble> getInLinks() {
+		return inLinks;
+	}
+	
+	public void addInLink(Bubble bubble) {
+		inLinks.add(bubble);
+	}
+	
+	public ArrayList<Bubble> getOutLinks() {
+		return outLinks;
+	}
+	
+	public void addOutLink(Bubble bubble) {
+		outLinks.add(bubble);
+	}
+	
+	public ArrayList<Bubble> getNestedBubbles() {
+		return nestedBubbles;
+	}
+	
+	public void addNestedBubble(Bubble bubble) {
+		nestedBubbles.add(bubble);
 	}
 
 }
--- PL2/PL2-shared/src/main/java/nl/tudelft/pl2016gr2/model/Node.java
+++ PL2/PL2-shared/src/main/java/nl/tudelft/pl2016gr2/model/Node.java
@@ -1,6 +1,5 @@
 package nl.tudelft.pl2016gr2.model;
 
-import java.util.ArrayList;
 
 /**
  * Mainly a dataholder class which represents a Node.
@@ -9,97 +8,22 @@ import java.util.ArrayList;
  */
 public class Node extends Bubble {
 	
-	private ArrayList<Edge> in;
-	private ArrayList<Edge> out;
 	private double flow;
-	private int id, bubbleStart, bubbleEnd;
-	private boolean isInDel;
 	
 	/**
 	 * Construct a new node.
 	 * @param id the id (index) of the node.
 	 */
 	public Node(int id) {
-		this.in = new ArrayList<Edge>();
-		this.out = new ArrayList<Edge>();
-		this.id = id;
+		super(id);
 		this.flow = 0;
-		this.bubbleStart = 0;
-		this.bubbleEnd = 0;
-		this.isInDel = false;
 	}
 	
 	@Override
 	public String toString() {
 		//return id + " inedges: " + in.size() + " outedges: " + out.size() + " flow: " + flow + " bubble (" + bubbleStart + ", " + bubbleEnd + ")" + " isInDel: " + isInDel;
-		return id + " level: " + getLevel();
-	}
-	
-	@Override
-	public boolean equals(Object object) {
-		if (object instanceof Node) {
-			Node node = (Node) object;
-			
-			return node.id == this.id;
-		}
-		
-		return false;
-	}
-	
-	/**
-	 * Add an incoming edge to the node.
-	 * @param e the incoming edge.
-	 */
-	public void addIn(Edge e) {
-		in.add(e);
-	}
-	
-	/**
-	 * Add an outgoing edge to the node.
-	 * @param e the outgoing edge.
-	 */
-	public void addOut(Edge e) {
-		out.add(e);
-	}
-	
-	public boolean isInDel() {
-		return isInDel;
-	}
-	
-	public void setInDel(boolean isInDel) {
-		this.isInDel = isInDel;
-	}
-	
-	public void setBubbleStart(int bubbleStart) {
-		this.bubbleStart = bubbleStart;
-	}
-	
-	public void setBubbleEnd(int bubbleEnd) {
-		this.bubbleEnd = bubbleEnd;
-	}
-
-	public ArrayList<Edge> getIn() {
-		return in;
-	}
-
-	public void setIn(ArrayList<Edge> in) {
-		this.in = in;
-	}
-
-	public ArrayList<Edge> getOut() {
-		return out;
-	}
-
-	public void setOut(ArrayList<Edge> out) {
-		this.out = out;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
+		//return id + " level: " + getLevel();
+		return getId() + " flow: " + this.flow;
 	}
 	
 	public double getFlow() {
