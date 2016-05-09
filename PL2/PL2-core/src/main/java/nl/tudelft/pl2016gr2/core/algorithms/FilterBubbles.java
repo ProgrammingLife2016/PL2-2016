06b8dced27c767b5621d0d179c701026fd6d56c6
Casper Athmer <casperathmer92@gmail.com>
2016-05-09 14:04:30 +0200

    Move dependency of newark from parser to shared

---
+++ PL2/PL2-core/src/main/java/nl/tudelft/pl2016gr2/core/algorithms/FilterBubbles.java
@@ -0,0 +1,17 @@
+package nl.tudelft.pl2016gr2.core.algorithms;
+
+import nl.tudelft.pl2016gr2.model.GraphInterface;
+import nl.tudelft.pl2016gr2.model.OriginalGraph;
+
+public class FilterBubbles {
+  
+  private GraphInterface graph;
+  private OriginalGraph originalGraph;
+  
+  public FilterBubbles(GraphInterface graph, OriginalGraph originalGraph) {
+    this.graph = graph;
+    this.originalGraph = originalGraph;
+  }
+  
+  
+}
