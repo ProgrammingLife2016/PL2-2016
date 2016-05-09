75e52ad93437d5afe5f96352ecaddd7ef66ae466
Casper Athmer <casperathmer92@gmail.com>
2016-05-09 12:50:51 +0200

    make class to filter straight sequences

---
+++ PL2/PL2-core/src/main/java/nl/tudelft/pl2016gr2/core/algorithms/FilterStraightSequences.java
@@ -0,0 +1,12 @@
+package nl.tudelft.pl2016gr2.core.algorithms;
+
+import nl.tudelft.pl2016gr2.model.GraphInterface;
+
+public class FilterStraightSequences {
+  
+  private GraphInterface graph;
+  
+  public FilterStraightSequences(GraphInterface graph) {
+    this.graph = graph;
+  }
+}
