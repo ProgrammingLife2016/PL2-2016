4253a49522cfa68c57b1acdd4c39bd59bb61fbe4
Justin van der Krieken <justin@vdkrieken.com>
2016-05-19 17:53:37 +0200

    Amended existing tests

--- PL2/PL2-gui/src/test/java/nl/tudelft/pl2016gr2/gui/view/graph/DrawGraphTest.java
+++ PL2/PL2-gui/src/test/java/nl/tudelft/pl2016gr2/gui/view/graph/DrawGraphTest.java
@@ -1,17 +1,21 @@
 package nl.tudelft.pl2016gr2.gui.view.graph;
 
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
 import javafx.scene.layout.Pane;
 import nl.tudelft.pl2016gr2.gui.javafxrunner.JavaFxJUnit4ClassRunner;
 import nl.tudelft.pl2016gr2.model.OriginalGraph;
 import nl.tudelft.pl2016gr2.parser.controller.GfaReader;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import java.io.File;
-
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import java.io.IOException;
 
 /**
  * This class tests the {@link DrawGraph} class.
@@ -21,13 +25,37 @@ import static org.mockito.Mockito.verify;
 @RunWith(JavaFxJUnit4ClassRunner.class)
 public class DrawGraphTest {
 
+  private static final String filename = "SMALL.gfa";
+
+  private File file;
+
+  /**
+   * Since the GfaReader uses files, we need to copy the resource to a file.
+   *
+   * @throws IOException When creation of tempfile fails.
+   */
+  @Before
+  public void setup() throws IOException {
+    file = File.createTempFile("GfaReaderTest", filename);
+
+    FileUtils.copyInputStreamToFile(
+        GfaReader.class.getClassLoader().getResourceAsStream(filename),
+        file);
+
+  }
+
+  @After
+  public void tearDown() {
+    file.delete();
+  }
+
   /**
    * Test of drawGraph method, of class DrawGraph.
    */
   @Test
   public void testDrawGraph() {
     Pane pane = spy(new Pane());
-    OriginalGraph graph = spy(new GfaReader(new File("SMALL.gfa")).read());
+    OriginalGraph graph = spy(new GfaReader(file).read());
 
     new DrawGraph().drawGraph(pane, graph);
     verify(pane, Mockito.atLeast(1)).getChildren();
--- PL2/PL2-parser/src/test/java/nl/tudelft/pl2016gr2/parser/controller/GfaReaderTest.java
+++ PL2/PL2-parser/src/test/java/nl/tudelft/pl2016gr2/parser/controller/GfaReaderTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import nl.tudelft.pl2016gr2.model.OriginalGraph;
 import nl.tudelft.pl2016gr2.thirdparty.testing.utility.AccessPrivate;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * This class tests the {@link GfaReader} class.
@@ -17,9 +21,31 @@ public class GfaReaderTest {
 
   private static final String filename = "SMALL.gfa";
 
+  private File file;
+
+  /**
+   * Since the GfaReader uses files, we need to copy the resource to a file.
+   *
+   * @throws IOException When creation of tempfile fails.
+   */
+  @Before
+  public void setup() throws IOException {
+    file = File.createTempFile("GfaReaderTest", filename);
+
+    FileUtils.copyInputStreamToFile(
+        GfaReader.class.getClassLoader().getResourceAsStream(filename),
+        file);
+
+  }
+
+  @After
+  public void tearDown() {
+    file.delete();
+  }
+
   @Test
   public void integrationTest() {
-    GfaReader reader = new GfaReader(new File(filename));
+    GfaReader reader = new GfaReader(file);
     OriginalGraph og = reader.read();
     assertEquals(og.getGenoms().size(), 11);
     assertEquals(og.getNode(3).getGenomes().size(), 1);
@@ -30,7 +56,7 @@ public class GfaReaderTest {
    */
   @Test
   public void testRead() {
-    GfaReader reader = new GfaReader(new File(filename));
+    GfaReader reader = new GfaReader(file);
     OriginalGraph actual = reader.read();
     OriginalGraph expected = AccessPrivate.getFieldValue("originalGraph", GfaReader.class, reader);
     assertEquals(expected, actual);
