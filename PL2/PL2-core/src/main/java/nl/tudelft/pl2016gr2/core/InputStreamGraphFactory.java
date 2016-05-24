82da70fe11300bb2e0c60475d69a248ae0351971
Justin van der Krieken <justin@vdkrieken.com>
2016-05-24 23:15:52 +0200

    Implement filechooser
    Implement filechooser. This filechooser is able to scan a directory
    for gfa/nwk/xlsx files to use for the application.
    
    Added open button to the File menu
    
    Also added factories for the graph+tree.
    These factories can be used to decouple the way
    code obtains its graph/tree.

---
+++ PL2/PL2-core/src/main/java/nl/tudelft/pl2016gr2/core/InputStreamGraphFactory.java
@@ -0,0 +1,26 @@
+package nl.tudelft.pl2016gr2.core;
+
+
+import nl.tudelft.pl2016gr2.model.SequenceGraph;
+import nl.tudelft.pl2016gr2.parser.controller.GfaReader;
+
+import java.io.InputStream;
+
+public class InputStreamGraphFactory implements GraphFactory {
+
+  private final InputStream stream;
+
+  public InputStreamGraphFactory(InputStream file) {
+    this.stream = file;
+  }
+
+  @Override
+  public SequenceGraph getGraph() {
+    return getGfaReader().read();
+  }
+
+  /*package*/ GfaReader getGfaReader() {
+    return new GfaReader(stream);
+  }
+
+}
--- PL2/PL2-core/src/test/java/nl/tudelft/pl2016gr2/core/algorithms/subgraph/CompareSubgraphsTest.java
+++ PL2/PL2-core/src/test/java/nl/tudelft/pl2016gr2/core/algorithms/subgraph/CompareSubgraphsTest.java
@@ -6,8 +6,14 @@ import nl.tudelft.pl2016gr2.model.NodePosition;
 import nl.tudelft.pl2016gr2.model.SequenceGraph;
 import nl.tudelft.pl2016gr2.parser.controller.GfaReader;
 import nl.tudelft.pl2016gr2.util.Pair;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,12 +24,37 @@ import java.util.HashMap;
  */
 public class CompareSubgraphsTest {
 
+  private static final String TEST_GRAPH_RESOURCE = "SMALL.gfa";
+  private GfaReader gfaReader;
+  private File file;
+
+  /**
+   * Initialize some testing variables.
+   *
+   * @throws IOException When file creation fails.
+   */
+  @Before
+  public void initialize() throws IOException {
+
+    file = File.createTempFile("GfaReaderTest", TEST_GRAPH_RESOURCE);
+    FileUtils.copyInputStreamToFile(
+        GfaReader.class.getClassLoader().getResourceAsStream(TEST_GRAPH_RESOURCE), file);
+
+    gfaReader = new GfaReader(new FileInputStream(file));
+  }
+
+  @After
+  public void tearDown() {
+    file.delete();
+  }
+
   /**
    * Test of compareGraphs method, of class CompareSubgraphs.
    */
   @Test
   public void testCompareGraphs() {
-    SequenceGraph graph = new GfaReader("SMALL.gfa").read();
+
+    SequenceGraph graph = gfaReader.read();
     GraphOrdererThread thread = new GraphOrdererThread(graph);
     thread.start();
     HashMap<Integer, NodePosition> mainGraphOrder = thread.getOrderedGraph();
